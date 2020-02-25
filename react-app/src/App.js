b4dd2ba404a657dd306392ee368a44f401d3db2f
Vyacheslav Zlivko <vzlivko.exceedteam@gmail.com>
2020-02-25 12:29:47 +0300

    little changes

---
+++ react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./header";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <div>Not header</div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ react-app/src/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h3>This is header and it will be showed on all pages</h3>
+      <p>__________________________________________________</p>
+    </header>
+  );
+}
+export default Header;
